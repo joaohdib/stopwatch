232391c7029f430a0a9da7da8302feb6e7461cc2
João Henrique Dib <112108662+joaohdib@users.noreply.github.com>
2024-06-20 16:26:30 -0300

    Update index.js
    js file adjusted

--- index.js
+++ index.js
@@ -1,16 +1,64 @@
-const botao = document.getElementById("botao");
-const min = 0;
-const maxX = 1500;
-const maxY = 800;
-let x;
-let y;
-botao.onmouseover = function() {
-    x = Math.round(Math.random() * (maxX - min) + min);
-    y = Math.round(Math.random() * (maxY - min) + min);
-    x = x.toString() + "px";
-    y = y.toString() + "px";
-    console.log(y);
-    botao.style.top = y;
-    botao.style.left = x;
-};
 
+const contador = document.getElementById('time');
+const resetButton = document.getElementById('reset');
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+let contadorInt = 0;
+let running = false;
+let date = new Date().getTime();
+let atualizarCont = 0;
+let contTemp = 0;
+let diferencaMs = 0;
+let diferencaMsAux;
+let primeiraVez = true;
+
+function atualiza() {
+    console.log(contTemp);
+    let newDate = new Date().getTime();
+    diferencaMs = Math.abs(newDate - date + contTemp);
+    diferencaMsAux = diferencaMs;
+
+    let horas = Math.floor(diferencaMs / (1000 * 60 * 60));
+    diferencaMs -= horas * (1000 * 60 * 60);
+
+    let minutos = Math.floor(diferencaMs / (1000 * 60));
+    diferencaMs -= minutos * (1000 * 60);
+
+    let segundos = Math.floor(diferencaMs / 1000);
+    diferencaMs -= segundos * 1000;
+
+    let milissegundos = diferencaMs;
+
+    let horasFormatadas = String(horas).padStart(2, '0');
+    let minutosFormatados = String(minutos).padStart(2, '0');
+    let segundosFormatados = String(segundos).padStart(2, '0');
+    let milissegundosFormatados = String(milissegundos).padStart(3, '0');
+
+contador.textContent = `${horasFormatadas}:${minutosFormatados}:${segundosFormatados}:${milissegundosFormatados}`;
+}
+
+
+startButton.onclick = function() {
+    if (running == false) {
+        running = true;
+        date = new Date().getTime();
+        atualizarCont = setInterval(atualiza, 1);
+    }
+
+}
+
+stopButton.onclick = function() {
+    contTemp = diferencaMsAux;
+    running = false;
+    clearInterval(atualizarCont);
+}
+
+resetButton.onclick = function() {
+    date = new Date();
+    contador.textContent = '00:00:00:000'
+    running = false;
+    contadorInt = 0;
+    contTemp = 0;
+    clearInterval(atualizarCont);
+    
+}
